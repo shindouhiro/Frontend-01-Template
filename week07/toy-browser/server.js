295b446882a2c9b13f07c16c5843123e53944d17
Hikaru <shindouhiro@gmail.com>
2020-05-31 23:35:44 +0800

    修改图

--- week07/toy-browser/server.js
+++ week07/toy-browser/server.js
@@ -7,49 +7,34 @@ const server = http.createServer((req, res) => {
   res.setHeader('X-Foo', 'bar');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end(`
-<html maaa=a >
-<head>
-<style>
-.background {
-  display: flex;
-  width: 800px;
-  height: 600px;
-  background-color: rgb(255,255,255);
-}
-.test {
-  display: flex;
-  width: 500px;
-  height: 400px;
-  background-color: rgb(253,226,30);
-  justify-content: center;
-  align-items: center;
-}
-.demo {
-  display: flex;
-  width: 200px;
-  height: 100px;
-  justify-content: center;
-  background-color: rgb(25,255,255);
-}
-.demo2 {
-  display: flex;
-  width: 100px;
-  height: 200px;
-  background-color: rgb(255,215,255);
-}
-</style>
-</head>
-<body>
-  <div id="id" class="background">
-    <div class="test">
-      <div class="demo"></div>
-      <div class="demo2"></div>
-    </div>
-    <img id="myid"/>
-    <img />
-  </div>
-</body>
-</html>
+  <html maaa=a >
+  <head>
+      <style>
+      #container {
+          width: 400px;
+          height: 400px;
+          display: flex;
+          background-color: rgb(255,255,255);
+      }
+      #container #myid {
+          width: 200px;
+          height: 100px;
+          background-color: rgb(255,182,193);
+      }
+      #container .c1 {
+          width: 200px;
+          flex: 1;
+          background-color: rgb(29,105,29);
+      }
+      </style>
+  </head>
+  <body>
+      <div id="container">
+        <div id="myid"></div>
+        <div class="c1"></div>
+      </div>
+  </body>
+ </html>
   `);
 })
 

777eef63e5c008a2d42fdd34574bcc63972c1354
hikaru <274173368@qq.com>
2020-12-08 14:27:27 +0800

    配置webpack 加入webpack-dev-server

--- week14/main.js
+++ week14/main.js
@@ -1,1 +1,14 @@
-const Cls = <Cls attr="123" />
+function create(Cls, attr) {
+    let o = new Cls()
+    for (let name in attr) {
+        o[name] = attr[name]
+    }
+    return o
+    // console.log(arguments)
+}
+class Div {
+
+}
+
+let component = <Div id="a" class="b" />
+console.log(component)
--- week14/webpack.config.js
+++ week14/webpack.config.js
@@ -1,23 +1,23 @@
-const path = require('path');
-
 module.exports = {
     entry: './main.js',
     module: {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
                 use: {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env'],
-                        plugins: [["@babel/plugin-transform-react-jsx", { pragma: "createElement" }]]
+                        plugins: [[
+                            "@babel/plugin-transform-react-jsx",
+                            { pragma: "create" }
+                        ]]
                     }
                 }
             }
         ]
     },
-    mode: 'development',
+    mode: "development",
     optimization: {
         minimize: false
     }
